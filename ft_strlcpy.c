5af8fe76267c7423456562ae62dd7c7c7090a666
moemarusan <moemaaaaaaaru@gmail.com>
2020-07-11 03:22:16 +0900

    :+1:[add] ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaiki <msaiki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 22:37:40 by msaiki            #+#    #+#             */
+/*   Updated: 2020/07/10 23:10:47 by msaiki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize)
+	{
+		while (*(src + i) && i < dstsize - 1)
+		{
+			*(dst + i) = *(src + i);
+			i++;
+		}
+		*(dst + i) = '\0';
+	}
+	return (ft_strlen(src));
+}
