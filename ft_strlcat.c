cffdefe417b79b8c003b268ea39068a54d431497
moemarusan <moemaaaaaaaru@gmail.com>
2020-07-14 14:09:28 +0900

    :+1:[add] strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaiki <msaiki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 03:22:56 by msaiki            #+#    #+#             */
+/*   Updated: 2020/07/14 13:42:09 by msaiki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dlen;
+	size_t	slen;
+	
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	if (dlen + 1 > dstsize)
+		return (slen + dstsize);
+	ft_strlcpy((dst + dlen), src, dstsize - dlen);
+	return (dlen + slen);
+}
--- libft.h
+++ libft.h
@@ -12,20 +12,20 @@ typedef struct	s_list
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
-int		ft_isascii(int n);
+int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int 	ft_toupper(int c);
 int 	ft_tolower(int c);
 int		ft_atoi(const char *str);
 // char	*ft_strdup(const char *s1);
-// size_t	ft_strlen(const char *str);
+size_t	ft_strlen(const char *s);
 // void	*ft_memset(void *b, int c, size_t len);
 // void	ft_bzero(void *s, size_t n);
 // void	*ft_memcpy(void *dst, const void *src, size_t n);
 // void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-// size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 // char	*ft_strchr(const char *s, int c);
 // char	*ft_strrchr(const char *s, int c);
 // int		ft_strncmp(const char *s1, const char *s2, size_t n);
