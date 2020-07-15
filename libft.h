db52426d1a4c938d0ed22897fe8144b9cce58da2
moemarusan <moemaaaaaaaru@gmail.com>
2020-07-16 08:09:32 +0900

    :+1:[add] strncmp

--- libft.h
+++ libft.h
@@ -28,7 +28,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-// int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // int		ft_min(int n1, int n2);
 // int		ft_max(int n1, int n2);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
