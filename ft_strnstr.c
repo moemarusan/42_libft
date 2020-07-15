568cf713b3ca3384f48b8716b3f41ab8192bed23
moemarusan <moemaaaaaaaru@gmail.com>
2020-07-15 23:58:23 +0900

    :+1:[add] strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msaiki <msaiki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 20:41:46 by msaiki            #+#    #+#             */
+/*   Updated: 2020/07/15 23:57:38 by msaiki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char *h;
+	char *n;
+	size_t	i;
+
+	h = (char *)haystack;
+	n = (char *)needle;
+	if (!*needle)
+		return (h);
+	while (*h && len)
+	{
+		i = 0;
+		while (h[i] == n[i] && *n && i < len)
+		{
+			if (n[i + 1] == '\0')
+				return (h);
+			i++;
+		}
+		len--;
+		h++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -31,7 +31,7 @@ char	*ft_strrchr(const char *s, int c);
 // int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // int		ft_min(int n1, int n2);
 // int		ft_max(int n1, int n2);
-// char	*strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 // void	*ft_calloc(size_t count, size_t size);
 // size_t	ft_strnlen(const char *s, size_t maxlen);
 // int		ft_isspace(char c);
