371c38f26f8c04148de2472cbe8836d253646967
moemarusan <moemaaaaaaaru@gmail.com>
2020-07-16 19:49:29 +0900

    :wrench:[fix] mallocでNULLではなく一意の謎のポインターを返されたときに滋賀ボートになるかもしれないので修正

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: msaiki <msaiki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 16:11:11 by msaiki            #+#    #+#             */
-/*   Updated: 2020/07/16 16:23:02 by msaiki           ###   ########.fr       */
+/*   Updated: 2020/07/16 19:48:51 by msaiki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	char *p;
 
-	if (!(p = malloc(count * size)))
+	if (!count || !size)
 		return (NULL);
+	p = malloc(count * size);
 	ft_bzero(p, count * size);
 	return (p);
 }
